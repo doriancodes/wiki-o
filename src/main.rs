dfa96df75b7cc7017a0467b17cd71ef19a1f1d50
doriancodes <dorian.alo.davi@gmail.com>
2024-03-13 01:21:26 +0100

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,22 @@ pub mod env;
 pub mod file;
 pub mod logging;
 
-use anyhow::Context;
+use std::path::PathBuf;
+
+use anyhow::Result;
 use env::{Environment, WContext};
 use home::home_dir;
 
 use crate::logging::show_config;
 
-fn main() {
+fn main() -> Result<()> {
     let matches = cli::cli().get_matches();
 
     let wcontext: WContext = WContext {
-        config_dir: format!("{}/.config/wiki-o", home_dir().unwrap().display()),
+        config_dir: format!("{}/.config/wiki-o", home_dir().get_or_insert(PathBuf::new()).display()),
     };
-    let config = wcontext.config().unwrap();
-    let notes_dir = wcontext.notes_abs_dir().unwrap();
+    let config = wcontext.config()?;
+    let notes_dir = wcontext.notes_abs_dir()?;
     let file_format: &String = &config.file_format;
 
     match matches.subcommand() {
@@ -28,7 +30,8 @@ fn main() {
                 _ => "my_notes".to_string(),
             };
 
-            action::add(content, &file_name, &notes_dir, file_format).unwrap(); //TODO handle nicely
+            action::add(content, &file_name, &notes_dir, file_format)?;
+            Ok(())
         }
         Some(("list", sub_matches)) => {
             let is_short: bool = match sub_matches.get_one::<String>("SHORT") {
@@ -36,21 +39,21 @@ fn main() {
                 _ => false,
             };
 
-            action::list(is_short, &notes_dir).unwrap(); //TODO handle nicely
+            action::list(is_short, &notes_dir)?;
+            Ok(())
         }
         Some(("delete", sub_matches)) => {
-            let file_name = sub_matches
-                .get_one::<String>("FILE")
-                .context("file name is required")
-                .unwrap();
-            action::delete(&notes_dir, &file_name, &file_format).unwrap(); //TODO handle nicely
+            let file_name = sub_matches.get_one::<String>("FILE").expect("required");
+            action::delete(&notes_dir, &file_name, &file_format)?;
+            Ok(())
         }
         Some(("purge", _)) => {
-            action::purge(&notes_dir).unwrap();
-            //TODO handle nicely
+            action::purge(&notes_dir)?;
+            Ok(())
         }
         Some(("config", _)) => {
             show_config("Current configuration: ".to_string(), config.to_string());
+            Ok(())
         }
         _ => unreachable!(),
     }
